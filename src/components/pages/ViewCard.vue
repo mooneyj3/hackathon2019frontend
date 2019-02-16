bf10920ce17024964df20a5861b75ac4d9bd8654
Sasha Puchko <alexandra.puchko@gmail.com>
2019-02-16 14:33:41 -0800

    ViewCard added

--- src/components/pages/ViewCard.vue
+++ src/components/pages/ViewCard.vue
@@ -1,13 +1,30 @@
 <template>
-    
+    <div>
+        <div class="card-container">
+            <Card></Card>
+        </div>
+        <div class="timeline-container">
+            <Timeline></Timeline>
+        </div>
+        </div>
 </template>
 
 <script>
+    import Timeline from "../timeline/Timeline";
+    import Card from "../cards/Card";
     export default {
-        name: "ViewCard"
+        name: "ViewCard",
+        components: {Timeline, Card}
     }
 </script>
 
 <style scoped>
+    .card-box{
+        margin-top:3%;
+    }
+    .timeline-container{
+        margin-left:5%;
+        margin-right:5%;
+    }
 
 </style>
