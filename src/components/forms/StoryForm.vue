d23323bbef90830ce8c4d161ea340f5e25c8ee71
Jonny Mooneyham <mooneyj3@wwu.edu>
2019-02-16 16:14:36 -0800

    Getting axios framework and router implemented

---
+++ src/components/forms/StoryForm.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-form >
+        <p>Share the story of how you received this card</p>
+        <!--<v-checkbox v-for="item in impact"-->
+                <!--v-model="checkbox"-->
+                <!--v-validate="'required'"-->
+                <!--:error-messages="errors.collect('checkbox')"-->
+                <!--value="1"-->
+                <!--label="item"-->
+                <!--data-vv-name="checkbox"-->
+                <!--type="checkbox"-->
+                <!--required>-->
+        <!--</v-checkbox>-->
+        <v-textarea
+                v-model="story"
+                :error-messages="nameErrors"
+                :counter="255"
+                maxlength="255"
+                full-width
+                single-line
+                label="Story"
+                @input="$v.name.$touch()"
+                @blur="$v.name.$touch()"
+        ></v-textarea>
+
+    </v-form>
+</template>
+
+<script>
+    export default {
+        name: "StoryForm",
+        data() {
+            return {
+                story: "",
+                impact: {
+                    made_day: "Made my day",
+                    changed_life: "Changed my life",
+                    restored_faith: "Restored my faith in humanity",
+                    meh: "Meh",
+                    kind_gesture: "What a kind gesture",
+                    feel_loved: "Made me feel loved",
+                },
+                made_day: false,
+                changed_life: false,
+                restored_faith: false,
+                meh: false,
+                kind_gesture: false,
+                feel_loved: false,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/pages/TellStory.vue
+++ src/components/pages/TellStory.vue
@@ -1,10 +1,85 @@
 <template>
-    
+    <div>
+        <!-- Card for form -->
+        <v-card v-if="show_form" max-width="600" min-width="400" >
+            <v-card-title primary-title >
+                <div >
+                    <h3 class="headline mb-0">Share Your Experience</h3>
+                    <StoryForm></StoryForm>
+                </div>
+            </v-card-title>
+            <v-card-actions>
+                <v-btn flat color="orange">Share</v-btn>
+                <v-btn flat color="orange">Explore</v-btn>
+            </v-card-actions>
+        </v-card>
+
+        <!-- Card for failed search -->
+        <v-card v-if="show_search">
+            <v-card-title primary-title>
+                <div >
+                    <h3 class="headline mb-0">Search for a card</h3>
+                    <div v-if="show_not_found">Uh oh, not found</div>
+                    <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+                </div>
+            </v-card-title>
+            <v-card-actions>
+                <v-btn flat color="orange">Share</v-btn>
+                <v-btn flat color="orange">Explore</v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
 </template>
 
 <script>
+    import StoryForm from "../forms/StoryForm";
     export default {
-        name: "TellStory.vue"
+        name: "TellStory.vue",
+        components: {StoryForm},
+        data() {
+            return {
+                query: this.$route.query,
+                show_form: false,
+                show_not_found: false,
+                show_search: false,
+                card_name: "",
+                card: "",
+            }
+        },
+        mounted: {},
+        computed: {},
+        methods: {
+            getCard() {
+                let self = this;
+
+                // check for query in url
+                if (!('name' in this.query)) {
+                    this.show_form = true;
+                }
+                else {
+                    axios.get(this.$searchAPI, {
+                        params: {
+                            cardName: self.query.name,
+                            oneOnly: true,
+                        }
+                    })
+                        .then(function(response) {
+                            if (response.data.length !== 1) {
+                                self.show_not_found = true;
+                            }
+                            self.show_form = true;
+                            self.card_name = self.query.name;
+                        })
+                        .catch(function(error) {
+                            self.show_not_found = true;
+                        })
+                }
+
+            }
+        },
+        beforeMount(){
+            this.getCard();
+        }
     }
 </script>
 
